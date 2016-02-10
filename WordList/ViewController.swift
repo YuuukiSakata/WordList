3620b3571555a334d8299eef849679b0ef78e877
Yuki Sakata <doragon3.0920@gmail.com>
2016-02-10 18:40:43 +0900

    Initial Commit

---
+++ WordList/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WordList
+//
+//  Created by Yuki Sakata on 2016/02/10.
+//  Copyright © 2016年 Yuki Sakata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
